80e09d1ad3143ef8bba698e84dc24f51d5925898
Darpan Dodiya <darpandodiya@gmail.com>
2022-02-19 20:17:44 -0800

    Create bixby-debloater.bat

---
+++ bixby-debloater.bat
@@ -0,0 +1,14 @@
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction
+adb shell pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.plmsync
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy
+adb shell pm uninstall -k --user 0 com.samsung.android.app.settings.bixby
+adb shell pm uninstall -k --user 0 com.samsung.systemui.bixby2
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.service
+adb shell pm uninstall -k --user 0 com.samsung.android.app.routines
+adb shell pm uninstall -k --user 0 com.samsung.android.visionintelligence
+adb shell pm uninstall -k --user 0 com.samsung.android.app.spage
